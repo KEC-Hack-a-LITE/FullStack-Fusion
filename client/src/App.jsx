5a34a2bca2d03276f6d5903b28e45753161523f5
Adwait Upadhyaya <adwait627@gmail.com>
2024-02-16 12:16:37 +0545

    footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,23 @@
-import React from 'react'
-import Header from './components/common/Header'
-import Home from './pages/Home'
-import Login from './pages/Login'
-import Signup from './pages/Signup'
-import { Routes ,Route} from 'react-router-dom'
+import React from "react";
+import Header from "./components/common/Header";
+import Footer from "./components/common/Footer";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import { Routes, Route } from "react-router-dom";
 
 export default function App() {
-  return <>
-  
-  <Header/>
- 
-  <Routes>
+  return (
+    <>
+      <Header />
 
-  <Route path='/' element={<Home/>}/>
-  <Route path='login' element={<Login/>}/>
-  <Route path='signup' element={<Signup/>}/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="login" element={<Login />} />
+        <Route path="signup" element={<Signup />} />
+      </Routes>
 
-
-  </Routes>
-  </> 
+      <Footer />
+    </>
+  );
 }
